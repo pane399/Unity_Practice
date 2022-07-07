694e8c5ae9474f808a397129b36152670d97198c
Yun, Jeong Hoon <pane399@korea.ac.kr>
2022-07-07 17:59:01 +0900

    2022.07.07 Commit " Unity practice - Quad Action "

---
+++ Quad_Action/BossRock.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossRock : Bullet
+{
+    Rigidbody rigid;
+    float angularPower = 2;
+    float scaleValue = 0.1f;
+    bool isShoot;
+
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody>();
+        StartCoroutine(GainPowerTimer());
+        StartCoroutine(GainPower());
+    }
+
+    IEnumerator GainPowerTimer()
+    {
+        yield return new WaitForSeconds(2.2f);
+        isShoot = true;
+    }
+
+    IEnumerator GainPower()
+    {
+        while (!isShoot)
+        {
+            angularPower += 0.02f;
+            scaleValue += 0.003f;
+            transform.localScale = Vector3.one * ( scaleValue > 2f ? 2f : scaleValue);
+            rigid.AddTorque(transform.right * angularPower, ForceMode.Acceleration);
+            yield return null;
+        }
+    }
+}
---
+++ Quad_Action/Weapon.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    public enum Type { Melee, Range }
+    public Type type;
+    public int damage;
+    public float rate;
+    public int maxAmmo;
+    public int curAmmo;
+
+    public BoxCollider meleeArea;
+    public TrailRenderer trailEffect;
+    public Transform bulletPos;
+    public GameObject bullet;
+    public Transform bulletCasePos;
+    public GameObject bulletCase;
+
+    public void Use()
+    {
+        if(type == Type.Melee)
+        {
+            StopCoroutine("Swing");
+            StartCoroutine("Swing");
+        }
+        else if (type == Type.Range && curAmmo > 0)
+        {
+            curAmmo--;
+            StartCoroutine("Shot");
+        }
+    }
+
+    IEnumerator Swing() //코루틴은 yield가 하나 이상 들어가야함
+    {
+        // yield return null; // 1프레임 대기
+
+        //1
+        yield return new WaitForSeconds(0.1f);
+        meleeArea.enabled = true;
+        trailEffect.enabled = true;
+        
+        //2
+        yield return new WaitForSeconds(0.3f);
+        meleeArea.enabled = false;
+
+        //3
+        yield return new WaitForSeconds(0.3f);
+        trailEffect.enabled = false;
+    }
+
+    IEnumerator Shot()
+    {
+        //1. 총알 발사
+        GameObject instantBullet = Instantiate(bullet, bulletPos.position, bulletPos.rotation);
+        Rigidbody bulletRigid = instantBullet.GetComponent<Rigidbody>();
+        bulletRigid.velocity = bulletPos.forward * 50;
+
+        yield return null;
+        //2. 탄피 배출
+        GameObject instantCase = Instantiate(bulletCase, bulletCasePos.position, bulletCasePos.rotation);
+        Rigidbody caseRigid = instantCase.GetComponent<Rigidbody>();
+        Vector3 caseVec = bulletCasePos.forward * Random.Range(-3, -2) + Vector3.up * Random.Range(2, 3);
+        caseRigid.AddForce(caseVec, ForceMode.Impulse);
+        caseRigid.AddTorque(Vector3.up * 10, ForceMode.Impulse);
+    }
+
+    //Use() 메인루틴 -> Swing()을 호출 서브루틴 끝난 후-> Use() 메인루틴
+    //Use() 메인루틴 + Swing() 코루틴 (함께 실행 Co-op)
+}
