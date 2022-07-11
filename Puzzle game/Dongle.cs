842ac0ce067946ad14d82bb8cec5d1ee6abe40d0
Yun, Jeong Hoon <pane399@korea.ac.kr>
2022-07-11 20:49:55 +0900

    22.07.11 Commit " Unity Practice - Puzzle game / Dongle "

---
+++ Puzzle game/Dongle.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dongle : MonoBehaviour
+{
+    public bool isDrag;
+    Rigidbody2D rigid;
+
+    void Awake() 
+    {
+        rigid = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if(isDrag) {
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            // X축 경계 설정
+            float leftBorder = -4.2f + transform.localScale.x / 2;
+            float rightBorder = 4.2f - transform.localScale.x / 2;
+
+            if(mousePos.x < leftBorder){
+                mousePos.x = leftBorder;
+         }
+            else if (mousePos.x > rightBorder){
+            mousePos.x = rightBorder;
+        }
+
+            mousePos.z = 0;
+            mousePos.y = 8;
+            transform.position = Vector3.Lerp(transform.position, mousePos, 0.2f);
+        }    
+    }
+
+    public void Drag()
+    {
+        isDrag = true;
+    }
+
+    public void Drop()
+    {
+        isDrag = false;
+        rigid.simulated = true;
+    }
+}
